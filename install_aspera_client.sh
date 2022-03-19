fb056940bda0c6ba003352200683ea5830c3ad8f
soumya/b/ghosh <soumyab.g@gmail.com>
2022-03-19 11:08:12 +0100

    Create install_aspera_client.sh

---
+++ install_aspera_client.sh
@@ -0,0 +1,3 @@
+curl https://d3gcli72yxqn2z.cloudfront.net/connect_latest/v4/bin/ibm-aspera-connect_4.1.1.73_linux.tar.gz >/tmp/ascp_linux.tar.gz
+tar xvfz /tmp/ascp_linux.tar.gz -C /tmp/
+sh /tmp/ibm-aspera-connect_4.1.1.73_linux.sh
